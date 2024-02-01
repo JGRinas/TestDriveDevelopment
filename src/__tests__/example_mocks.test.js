1a8b1c49635269a334c7016fc659e6bfd763550f
Juan Gabriel Benitez Rinas <juangabrielrinas@gmail.com>
2024-02-01 12:30:39 -0300

    added more examples of tests with mocks

--- src/__tests__/example_mocks.test.js
+++ src/__tests__/example_mocks.test.js
@@ -1,3 +1,8 @@
+import { storage } from "../lib/storage";
+import { saveUsername, getUsername } from "../user";
+
+jest.mock('../lib/storage')
+
 test('first example', ()=> {
     const myMock = jest.fn().mockReturnValueOnce(true).mockReturnValueOnce('hello world').mockReturnValueOnce(5);
     
@@ -10,4 +15,20 @@ test('first example', ()=> {
     expect(resutl1).toBe(true)
     expect(resutl2).toBe('hello world')
     expect(resutl3).toBe(5)
+})
+
+test('second example', ()=>{
+    saveUsername('juan')
+    expect(storage.save).toHaveBeenCalled();
+    expect(storage.save).toHaveBeenCalledWith({key: 'username', value: 'juan'})
+})
+
+test('thirt example', ()=>{
+    const username = 'juan'
+    storage.get.mockReturnValueOnce(username)
+    const result = getUsername()
+
+    expect(result).toBe(username)
+    expect(storage.get).toHaveBeenCalled();
+    expect(storage.get).toHaveBeenCalledWith({key: 'username', value: 'juan'})
 })
